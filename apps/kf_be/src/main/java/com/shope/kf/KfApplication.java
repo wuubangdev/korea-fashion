9cce91f7b0ae90fdc04b58fd2ebdf63bf8ca6acb
wuubangdev <wuubangdev@gmail.com>
2026-05-21 14:06:25 +0700

    feat(root): initialize monorepo with spring boot (kf_be) and nextjs (kf_fe)

---
+++ apps/kf_be/src/main/java/com/shope/kf/KfApplication.java
@@ -0,0 +1,13 @@
+package com.shope.kf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KfApplication.class, args);
+	}
+
+}
---
+++ apps/kf_be/src/test/java/com/shope/kf/KfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shope.kf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
